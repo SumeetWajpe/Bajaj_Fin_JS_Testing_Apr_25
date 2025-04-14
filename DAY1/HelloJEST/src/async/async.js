371d32bc0f365ddf9aef62289f70decdd5227bbc
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-04-14 15:10:49 +0530

    ✨ Functions returning promise

--- DAY1/HelloJEST/src/async/async.js
+++ DAY1/HelloJEST/src/async/async.js
@@ -1,10 +1,14 @@
 const axios = require("axios");
 
 const fetchPost = async id => {
+  console.log("Within Fetch Post");
   const results = await axios.get(
     `https://jsonplaceholder.typicode.com/posts/${id}`,
   );
   return results.data;
 };
 
-module.exports = fetchPost;
+const fetchPostToReturnAPromise = id => {
+  return axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+};
+module.exports = { fetchPost, fetchPostToReturnAPromise };
--- DAY1/HelloJEST/src/async/async.spec.js
+++ DAY1/HelloJEST/src/async/async.spec.js
@@ -1,14 +1,24 @@
-const fetchPost = require("./async");
+const postModule = require("./async");
 describe("call fetchPost to fetch post data", () => {
-  it("", async () => {
+  it("checks if post with correct id is returned", async () => {
     try {
-      const post = await fetchPost(10);
-      expect(post).toEqual({
+      //arrange
+      const mockResult = {
         userId: 1,
         id: 10,
         title: "optio molestias id quia eum",
         body: "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error",
-      });
+      };
+      //act
+      const post = await postModule.fetchPost(10);
+      // assert
+      expect(post).toEqual(mockResult);
     } catch (error) {}
+  });
+
+  xit("handles the method that returns a promise", () => {
+    return expect(
+      postModule.fetchPostToReturnAPromise(10),
+    ).resolves.toBeDefined();
   });
 });
