a363b1af6e94a791176d1a168326ce3fff9d7e51
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2025-04-17 10:16:13 +0530

    ✨ using before to fetch fixtures before tests run

--- DAY3_Cypress/CypressBasics/cypress/e2e/lambdatest.cy.js
+++ DAY3_Cypress/CypressBasics/cypress/e2e/lambdatest.cy.js
@@ -1,19 +1,33 @@
 /// <reference types="Cypress" />
 describe("test suite for lambdatest.io", () => {
-  it("should login successfully", () => {
+  before(function () {
+    cy.fixture("login").then(function (data) {
+      this.loginData = data;
+    });
+  });
+
+  it("should login successfully", function () {
     cy.visit(
       "https://ecommerce-playground.lambdatest.io/index.php?route=account/login",
     );
-
-    cy.fixture("login").then(data => {
-      cy.log(data);
-      cy.get("#input-email").type(data.name);
-      cy.get("#input-password").type(data.pwd);
-      cy.get("form > .btn").click();
-      cy.url().should(
-        "eq",
-        "https://ecommerce-playground.lambdatest.io/index.php?route=account/account",
-      );
-    });
+    cy.log(this.loginData);
+    cy.get("#input-email").type(this.loginData.name);
+    cy.get("#input-password").type(this.loginData.pwd);
+    cy.get("form > .btn").click();
+    cy.url().should(
+      "eq",
+      "https://ecommerce-playground.lambdatest.io/index.php?route=account/account",
+    );
   });
+  // 1st way in .then()
+  // cy.fixture("login").then(data => {
+  //   cy.log(data);
+  //   cy.get("#input-email").type(data.name);
+  //   cy.get("#input-password").type(data.pwd);
+  //   cy.get("form > .btn").click();
+  //   cy.url().should(
+  //     "eq",
+  //     "https://ecommerce-playground.lambdatest.io/index.php?route=account/account",
+  //   );
+  // });
 });
